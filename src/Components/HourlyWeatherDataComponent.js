12ba2fce069696703d4394a6281484b96489cf2e
debroy973-eng <debojyotibackup@gmail.com>
2022-09-27 00:31:15 +0530

    upadted pages

--- src/Components/HourlyWeatherDataComponent.js
+++ src/Components/HourlyWeatherDataComponent.js
@@ -5,7 +5,7 @@ function HourlyWeatherDataComponent(props) {
   let urlImage="https://openweathermap.org/img/wn/"+props.data.weather[0].icon+"@2x.png";
   let dateObj=new Date(props.data.dt*1000);
   return (
-      <div className="container border border-dark rounded" style={{'background-color': 'rgb(248, 223, 190)'}}>
+      <div className="container border border-dark rounded" style={{'backgroundColor': 'rgb(248, 223, 190)'}}>
             <div className="container text-center">
                 <div className="row">
                     <div className="col">
